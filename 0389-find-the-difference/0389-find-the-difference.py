971e4c2ad79b09e12c87c18c1f00dcaf6f6f58c9
shadirvan <141410453+Shadirvan1@users.noreply.github.com>
2026-01-15 16:38:12 +0530

    Time: 0 ms (100%), Space: 12.4 MB (92.05%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        for i in t:
+            if s.count(i) != t.count(i):
+                return i
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        
+        
+
+        
