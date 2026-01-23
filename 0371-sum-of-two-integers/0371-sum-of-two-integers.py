c58e900ddb5aa5d5a39503dc8308f389e4c90d56
shadirvan <141410453+Shadirvan1@users.noreply.github.com>
2026-01-23 16:21:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (98.65%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,6 +1,9 @@
 class Solution(object):
     def getSum(self, a, b):
-        return a+b
+        l = []
+        l.append(a)
+        l.append(b)
+        return sum(l)
         """
         :type a: int
         :type b: int
