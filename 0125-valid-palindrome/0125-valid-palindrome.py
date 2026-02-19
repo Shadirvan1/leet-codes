cb62625ab9a70c5f729a66c37b0cf9f7304da42a
shadirvan <141410453+Shadirvan1@users.noreply.github.com>
2026-02-19 15:53:17 +0530

    Time: 8 ms (95.15%), Space: 14.2 MB (19.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,9 +3,9 @@ class Solution(object):
         s=s.lower()
        
         d =[x for x in s if x.isalpha() or x.isdigit()]
-        print(d)
+  
         dd = d[::-1]
-        print(dd)
+ 
         if d == dd:
             return True
         else:
