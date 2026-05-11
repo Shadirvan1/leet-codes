fd40b190eff584d937fd680022e9d3b5a561a50d
shadirvan <141410453+Shadirvan1@users.noreply.github.com>
2026-05-11 09:29:05 +0530

    Time: 4 ms (25.2%), Space: 12.4 MB (19.69%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def canAliceWin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s=0
+        d=0
+        ss = []
+        for i in nums:
+            if len(str(i)) == 1:
+                s += i
+            else:
+                d += i
+        if s > d or d > s:
+            return True
+        else:
+            return False
